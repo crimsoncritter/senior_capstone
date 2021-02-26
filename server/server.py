3baab93932b162a5058d3c8a0ef86aa9646ee0b9
joshuabrudnak <joshuabrudnak@oakland.edu>
2021-02-26 13:09:47 -0500

    Add inital endpoints to post,get,remove cart requests

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ from os import path, system
 from flask import Flask, jsonify, request, redirect, url_for
 import pyrebase
 import server_init
+import uuid
 
 firebase_config = server_init.get_firebase_config()
 firebase = pyrebase.initialize_app(firebase_config)
@@ -29,8 +30,8 @@ def create_user():
         fb_data["working"] = True
         fb_data["store_id"] = content["store_id"]
 
-    auth.create_user_with_email_and_password(fb_data["email"], fb_data["password"])
-    db.child("users").push(fb_data)
+    user = auth.create_user_with_email_and_password(fb_data["email"], fb_data["password"])
+    db.child("users").push(fb_data, user['idToken'])
     print(fb_data)
 
     return login(fb_data["email"], fb_data["password"])
@@ -43,10 +44,41 @@ def auth_user():
     password = content["password"]
     return login(email, password)
 
-@app.route('/user/auth/expire', methods=['POST'])
-def expire_user(auth_data):
-    content = request.json
-    # firebase.auth()
+@app.route('/user/cart-request', methods=['POST'])
+def new_cart_request(auth_data):
+    request_data = request.get_json()
+    user_id = request.get_json()["user_id"]
+    cart_request = request.get_json()["cart_request"]
+
+    user = db.child("user").get(token=user_id)
+
+    if user == None:
+        return "User does not exist", 401
+    db.child("cart_request").child(str(uuid.uuid4())).push(cart_request)
+
+@app.route('/user/cart-request', methods=['GET'])
+def get_cart_requests(auth_data):
+    user_id = request.get_json()["user_id"]
+    user = db.child("user").get(token=user_id)
+
+    if user == None:
+        return "User does not exist", 401
+    elif user["role"] == "Employee" or user["role"] == "admin":
+        return db.child("cart_request").child().get()
+    else:
+        return "User does not have access", 403
+
+@app.route('/user/cart-request', methods=['DELETE'])
+def remove_cart_requests(auth_data):
+    user_id = request.get_json()["user_id"]
+    user = db.child("user").get(token=user_id)
+
+    if user == None:
+        return "User does not exist", 401
+    elif user["role"] == "Employee" or user["role"] == "admin":
+        db.child("cart_request").child(request_id).remove()
+    else:
+        return "User does not have access", 403
 
 if __name__ == '__main__':
     app.run(debug=True)
