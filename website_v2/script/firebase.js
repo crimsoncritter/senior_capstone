662f1bfc7295a6b74be676eb22bbbb233ecb4ece
joshuabrudnak <joshuabrudnak@oakland.edu>
2021-03-28 12:50:37 -0400

    Check user auth

--- website_v2/script/firebase.js
+++ website_v2/script/firebase.js
@@ -9,16 +9,35 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var auth = firebase.auth();
+var userId = "";
+
+function is_auth() {
+  const uid = sessionStorage.getItem("uid");
+  return firebase.auth().currentUser.uid == uid;
+}
 
 function login() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   auth.signInWithEmailAndPassword(email, password).then(
     function (user) {
-      console.log(user);
+      sessionStorage.setItem("uid", user.uid);
+      window.location = "./index.html";
     },
     function (e) {
       console.error(e.message);
     }
   );
+}
+
+function stores() {
+  if (is_auth) {
+    let storeRef = firebase.database().ref("Stores");
+    storeRef.on("value", (res) => {
+      let data = res.val();
+      console.log(data);
+    });
+  } else {
+    window.location = "./login.html";
+  }
 }
