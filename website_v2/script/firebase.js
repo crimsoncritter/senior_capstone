6d365d67379c62e07c099c873bf18be3ff28785f
joshuabrudnak <joshuabrudnak@oakland.edu>
2021-03-28 12:16:05 -0400

    Add basic user login

---
+++ website_v2/script/firebase.js
@@ -0,0 +1,24 @@
+const config = {
+  apiKey: "AIzaSyCJx7wxQxatZvbv34uiJRXvtSJuBn1jIiI",
+  authDomain: "cart-assist-8dc13.firebaseapp.com",
+  databaseURL: "https://cart-assist-8dc13-default-rtdb.firebaseio.com/",
+  storageBucket: "cart-assist-8dc13.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var auth = firebase.auth();
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  auth.signInWithEmailAndPassword(email, password).then(
+    function (user) {
+      console.log(user);
+    },
+    function (e) {
+      console.error(e.message);
+    }
+  );
+}
